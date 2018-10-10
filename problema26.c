7cedfcbb48d429cecfaa5375ae7861d0a0ee4411
TTinan <martirubioestrada@gmail.com>
2018-10-10 16:19:37 +0200

    Possiblement acabat Jacobi

--- problema26.c
+++ problema26.c
@@ -78,7 +78,7 @@ int matrix_postion(int i, int j, int n)
 /**
 * Funció que retorna el valor de la matriu en la posició (i,j)
 */
-double matrix_bj_postion(int i, int j)
+double matrix_bj_postion(int i, int j, int n)
 {
     if(abs(i-j) == 2){
         if(i%2 == 0){
@@ -116,7 +116,7 @@ double vector_position(int i, int n)
     }
 }
 
-int U_mes_L(int i, int j){
+int U_mes_L(int i, int j, int n){
     if (abs(i-j) == 2){
         return -1;
     }
@@ -151,7 +151,7 @@ double* U_mes_L_mult(double* v, int n)
     vector = fill_vector_solution(n);
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            vector[i] += v[j]*U_mes_L(i,j);
+            vector[i] += v[j]*U_mes_L(i,j, n);
         }
     }
     return vector;
@@ -161,6 +161,7 @@ double* U_mes_L_mult(double* v, int n)
 void jacobi_method(double *vector_solution, double *vector_solution_ant, double *vectorb, int n, double max_error, double modul_Bj)
 {
     double error = 1;
+    printf("%f\n", error);
     while (error > max_error){
         vector_solution_ant = vector_solution;
         vector_solution = U_mes_L_mult(vector_solution, n);
@@ -168,7 +169,8 @@ void jacobi_method(double *vector_solution, double *vector_solution_ant, double 
             vector_solution[i] = vectorb[i] - vector_solution[i];
         }
         vector_solution = D_inversa_mult(vector_solution, n);
-        error = infinite_norm(vector_solution, vector_solution_ant);
+        error = infinite_norm(vector_solution, vector_solution_ant, n);
+        printf("%f\n", error);
     }
 }
 
@@ -182,16 +184,17 @@ int main()
     double* vector_solution;
     double* vector_solution_ant;
     double modul_Bj = 2./3.;
-    
+    printf("Creades variables\n");
+
     // Omplim la matriu
     //matrixA = fill_matrix(dimension);
 
     // Omplim el vector
     vectorb = fill_vector(dimension);
     vector_solution = fill_vector_solution(dimension);
-    vector_ant = fill_vector_solution(dimension);
+    vector_solution_ant = fill_vector_solution(dimension);
 
-
+    printf("Comencem Jacobi\n");
     jacobi_method(vector_solution, vector_solution_ant, vectorb, dimension, max_error, modul_Bj);
     // Alliberem memòria
     int i = 0;
