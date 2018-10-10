f7de4c13ed945e907b5501a0be99c39cb6674f91
TTinan <martirubioestrada@gmail.com>
2018-10-10 22:20:28 +0200

    Jacobi acabat (a falta de comprovar resultats)

--- problema26.c
+++ problema26.c
@@ -12,8 +12,8 @@ double* fill_vector(int n)
     int i = 0;
     for(i = 0; i<n; i++){
         if (i%2 == 0){
-            vectorb[i] = (i+2)/n;
-            vectorb[i+1] = (i+2)/n;
+            vectorb[i] = (i+2.)/n;
+            vectorb[i+1] = (i+2.)/n;
         }
     }
     return vectorb;
@@ -109,23 +109,23 @@ double matrix_bj_postion(int i, int j, int n)
 double vector_position(int i, int n)
 {
     if(i%2 == 0){
-        return (i+2)/n;
+        return (i+2.)/n;
     }
     else{
-        return (i+1)/n;
+        return (i+1.)/n;
     }
 }
 
 int U_mes_L(int i, int j, int n){
     if (abs(i-j) == 2){
-        return -1;
+        return -1.;
     }
     else{
         if(abs(i-j) == n-1){
-            return 1;
+            return 1.;
         }
         else{
-            return 0;
+            return 0.;
         }
     }
 }
@@ -136,10 +136,10 @@ double* D_inversa_mult(double* v, int n)
     vector = fill_vector_solution(n);
     for(int i = 0; i < n; i++){
         if(i % 2 == 0){
-            vector[i] = v[i]/3;
+            vector[i] = v[i]/3.;
         }
         else{
-            vector[i] = v[i]/4;
+            vector[i] = v[i]/4.;
         }
     }
     return vector;
@@ -150,9 +150,25 @@ double* U_mes_L_mult(double* v, int n)
     double* vector;
     vector = fill_vector_solution(n);
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            vector[i] += v[j]*U_mes_L(i,j, n);
+        if (i < 2){
+            vector[i] += U_mes_L(i,i+2,n)*v[i+2];
+            vector[i] += U_mes_L(i,n+1-i,n)*v[n+1-i];
         }
+        else{
+            if (i == n - 2){
+                vector[i] += U_mes_L(i,i-2,n)*v[i-2];
+                vector[i] += U_mes_L(i,0,n)*v[0];
+            }
+            else if (i == n-1){
+                vector[i] += U_mes_L(i,i-2,n)*v[i-2];
+                vector[i] += U_mes_L(i,1,n)*v[1];
+            }
+            else{
+                vector[i] += U_mes_L(i,i-2,n)*v[i-2];
+                vector[i] += U_mes_L(i,i+2,n)*v[i+2];
+            }
+        }
+        
     }
     return vector;
 }
@@ -161,7 +177,9 @@ double* U_mes_L_mult(double* v, int n)
 void jacobi_method(double *vector_solution, double *vector_solution_ant, double *vectorb, int n, double max_error, double modul_Bj)
 {
     double error = 1;
-    printf("%f\n", error);
+    int Iteration = 1;
+    printf("Iteration: %i\n", Iteration);
+    // printf("%f\n", error);
     while (error > max_error){
         vector_solution_ant = vector_solution;
         vector_solution = U_mes_L_mult(vector_solution, n);
@@ -170,7 +188,9 @@ void jacobi_method(double *vector_solution, double *vector_solution_ant, double 
         }
         vector_solution = D_inversa_mult(vector_solution, n);
         error = infinite_norm(vector_solution, vector_solution_ant, n);
-        printf("%f\n", error);
+        // printf("E:    %f *10^-9\n", error*1000000000);
+        Iteration++;
+        printf("%i\n", Iteration);
     }
 }
 
@@ -184,7 +204,6 @@ int main()
     double* vector_solution;
     double* vector_solution_ant;
     double modul_Bj = 2./3.;
-    printf("Creades variables\n");
 
     // Omplim la matriu
     //matrixA = fill_matrix(dimension);
@@ -194,10 +213,19 @@ int main()
     vector_solution = fill_vector_solution(dimension);
     vector_solution_ant = fill_vector_solution(dimension);
 
-    printf("Comencem Jacobi\n");
+    printf("\n\n********************\n");
+    printf("*                  *\n");
+    printf("* MÈTODE DE JACOBI *\n");
+    printf("*                  *\n");
+    printf("********************\n");
     jacobi_method(vector_solution, vector_solution_ant, vectorb, dimension, max_error, modul_Bj);
     // Alliberem memòria
-    int i = 0;
+
+    printf("\n\n**************************\n");
+    printf("*                        *\n");
+    printf("* MÈTODE DE GAUSS-SEIDEL *\n");
+    printf("*                        *\n");
+    printf("**************************\n");
     /*
     for(i = 0; i < dimension; i++){
         free(matrixA[i]);
